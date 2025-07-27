38311f5cd1c174ec93a78b05f100ec8409ef9846
Shahnawaz-attar <shahnawazttar55@gmail.com>
2025-07-27 20:29:11 +0530

    feat: add dashboard layout and sidebar components
    - Introduced a new layout component for the dashboard that includes a sidebar.
    - Created a dashboard sidebar with navigation links and user button.
    - Added a trial component to display free usage statistics.
    - Implemented user authentication and logout functionality in the sidebar.
    - Integrated generated avatars using DiceBear for user representation.
    - Updated global styles for sidebar and primary colors.
    - Modified sign-in and sign-up views to use new sidebar styles.

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import React from "react";
-import HomeView from "./modules/home/ui/views/home-view";
+import HomeView from "../modules/home/ui/views/home-view";
 
 const Page = async () => {
   const session = await auth.api.getSession({
--- src/app/modules/auth/ui/views/sign-in-view.tsx
+++ src/app/modules/auth/ui/views/sign-in-view.tsx
@@ -178,7 +178,7 @@ export const SignInView = () => {
 
           <div
             className="
-          bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center
+          bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center
           "
           >
             <Image
